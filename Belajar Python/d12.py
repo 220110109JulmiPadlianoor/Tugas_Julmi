a5514a0397e5147f25bb4f8b721996b9c8759940
220110109JulmiPadlianoor <jpadlianoor@gmail.com>
2024-08-10 09:54:26 +0800

    Add files via upload

---
+++ Belajar Python/d12.py
@@ -0,0 +1,20 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> dimensi1=range(60,111)
+>>> dimensi2=range(12,18)
+>>> d1=dimensi1
+>>> d2=dimensi2
+>>> print()
+
+>>> print((d1[0],d2[0]));(d1[1],d2[1]);(d1[2],d2[2]);(d1[3],d2[3]);(d1[4],d2[4]);(d1[5],d2[5])
+(5, 12)
+(6, 13)
+(7, 14)
+(8, 15)
+(9, 16)
+(10, 17)
+>>> print()
+
+>>> print("terima kasih")
+terima kasih
+>>> 
